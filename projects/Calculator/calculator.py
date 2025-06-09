bcdc130b3b3e9687b288008c9af225d6f76feb01
jihane01 <jihanemajd14.com>
2025-06-09 22:06:51 +0100

    add the Calculator

---
+++ projects/Calculator/calculator.py
@@ -0,0 +1,17 @@
+num1 = int(input('Enter first number : '))
+num2 = int(input('Enter Second number : '))
+
+op = input('Enter the Operator  ')
+
+if op ==  '+' :
+    print(num1+num2)
+elif op == '-':
+        print(num1-num2)
+elif op == '*':
+      print(num1 *num2)
+elif op == '/':
+      print(num1/num2)
+else:
+      print('Inavalid Operator Enter + or - or * or /')        
+
+
